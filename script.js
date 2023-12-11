9b85c4ac4aa43b490b7fefc12b518be97f39d935
bryant tunbutr <bryantt23@users.noreply.github.com>
2023-12-10 23:01:45 -0800

    build tree

---
+++ script.js
@@ -0,0 +1,60 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor(arr) {
+    arr.sort((a, b) => a - b);
+    this.root = this.buildTree(arr);
+  }
+  buildTree = arr => {
+    let i = 0,
+      n = arr.length,
+      j = n - 1;
+    return this.buildTreeHelper(arr, i, j);
+  };
+  buildTreeHelper = (arr, i, j) => {
+    //bc
+    if (i > j) {
+      return null;
+    }
+    const mid = i + Math.floor((j - i) / 2);
+    const node = new Node(arr[mid]);
+    node.left = this.buildTreeHelper(arr, i, mid - 1);
+    node.right = this.buildTreeHelper(arr, mid + 1, j);
+    return node;
+  };
+  prettyPrint = (node = this.root, prefix = '', isLeft = true) => {
+    if (node === undefined || node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? '│   ' : '    '}`,
+        false
+      );
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+  };
+}
+
+let arr = [1, 2, 3];
+let tree = new Tree(arr);
+tree.prettyPrint();
+arr = [57, 8, 33, 55, 29, 7, 72, 9, 71, 20];
+tree = new Tree(arr);
+tree.prettyPrint();
+arr = [
+  27, 53, 98, 69, 40, 62, 55, 85, 70, 44, 97, 15, 32, 65, 73, 39, 43, 36, 30,
+  68, 33
+];
+tree = new Tree(arr);
+tree.prettyPrint();
